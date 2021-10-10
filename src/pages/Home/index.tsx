33fcf1cc8c17e9146eee058c8471af885033e876
Rubens Batista <batistarubinho@gmail.com>
2021-10-10 01:09:45 -0300

    add behoviour to cart page with products info

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -68,7 +68,7 @@ const Home = (): JSX.Element => {
           >
             <div data-testid="cart-product-quantity">
               <MdAddShoppingCart size={16} color="#FFF" />
-              {cartItemsAmount[product.id] || 0} 2
+              {cartItemsAmount[product.id] || 0}
             </div>
 
             <span>ADICIONAR AO CARRINHO</span>
